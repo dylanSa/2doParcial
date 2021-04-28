3deaf2671b53cd2f159df1085123f50705545fcd
dylanSa <78856890+dylanSa@users.noreply.github.com>
2021-04-28 11:27:44 -0500

    Add files via upload

---
+++ Parcial/script.js
@@ -0,0 +1,14 @@
+ // Activación de transición del menú principa/* Activador de jQuery */
+$(document).ready(function(){
+    // Activador de Slick Slider
+    $('.slick-banner').slick({
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        autoplaySpeed: 2000,
+        speed: 700,
+        infinite: true,
+        arrows: true
+    });
+});
